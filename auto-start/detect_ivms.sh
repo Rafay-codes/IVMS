14e21231c03c14825125b49bd4af13f5d9d05529
Abdul Aziz Mahaboob Basha <abdulaziz.m.eie@gmail.com>
2025-07-01 17:10:25 +0400

    Revert "Initial commit"
    This reverts commit e2007f5c2d067cbe9e8134646e0d5f8eaa3a4a81.

--- auto-start/detect_ivms.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# https://forums.developer.nvidia.com/t/trouble-initializing-nvtracker-lib/269893/5?u=smartguy9996
-export LD_PRELOAD=/opt/nvidia/cupva-2.3/lib/aarch64-linux-gnu/libcupva_host.so.2.3.0
-source /srv/ivms_venv/bin/activate
-cd /srv/ivms/detect
-python3 detect_ivms.py 
